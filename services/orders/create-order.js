831fff0572b1005c7d98e8ef798dfe28587aff8e
Fannn03 <fann.fann771@gmail.com>
2023-10-16 00:11:56 +0700

    feat: create form builder order

---
+++ services/orders/create-order.js
@@ -0,0 +1,18 @@
+import { ModalBuilder } from 'discord.js'
+import builderForm from '../../core/forms/orders/builder.js'
+
+export default async (interaction) => {
+  const { customId } = interaction
+
+  let modal = new ModalBuilder()
+    .setCustomId('builder-form')
+    .setTitle('Complete Builder Form')
+  
+  let buttons   
+  if(customId == 'builder') {
+    buttons = builderForm()
+    modal.addComponents(buttons.fullname, buttons.phone, buttons.address, buttons.notes)
+  }
+
+  return await interaction.showModal(modal)
+}
