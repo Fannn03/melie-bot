01949fad0d11998a959daad022cc4a0cde14f31a
Fannn03 <fann.fann771@gmail.com>
2023-10-16 09:14:15 +0700

    ref: display role required permission

--- helper/permission-message.js
+++ helper/permission-message.js
@@ -1,12 +1,24 @@
-import { EmbedBuilder } from "discord.js"
+import { EmbedBuilder, roleMention } from "discord.js"
 
-export default (message) => {
+export default async(message, permissions) => {
+
+  let roles = ''
+  await permissions.map(role => {
+    const findRole = message.guild.roles.cache.find(data => data.name === role)
+    roles += roleMention(findRole.id)
+  })
+
+  let text = ''
+
+  text += "You don't have permission to use this commands\n\n"
+  text += `**Required roles :** ${roles}`
+
   return message.reply({
     embeds: [
       new EmbedBuilder()
         .setColor('Red')
         .setTitle('Missing permissions')
-        .setDescription("You don't have permission to use this commands")
+        .setDescription(text)
     ]
   })
 }
