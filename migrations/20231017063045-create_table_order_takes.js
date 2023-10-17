c6ce96b651573f309b274457ae46804c891d425f
Fannn03 <fann.fann771@gmail.com>
2023-10-17 15:58:27 +0700

    feat: create migration table order takes

---
+++ migrations/20231017063045-create_table_order_takes.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('order_takes', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      order_id: {
+        type: Sequelize.INTEGER,
+        unique: true,
+        references: {
+          model: 'orders',
+          key: 'id'
+        },
+        allowNull: false
+      },
+      user_id: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      price: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+        allowNull: false
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('order_takes')
+  }
+};
