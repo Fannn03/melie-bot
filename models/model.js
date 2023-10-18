e04a25c1f385e3cd272e0648decc2a9451252614
Fannn03 <fann.fann771@gmail.com>
2023-10-18 08:06:43 +0700

    feat: associace order model with take ordeer

--- models/model.js
+++ models/model.js
@@ -1,11 +1,16 @@
 import service from './service.js'
 import order from './order.js'
+import takeOrder from './take-order.js'
 
 service.hasMany(order, {
   foreignKey: 'service_id'
 })
+order.hasOne(takeOrder, {
+  foreignKey: 'order_id'
+})
 
 export default {
   service: service,
-  order: order
+  order: order,
+  takeOrder: takeOrder
 }
